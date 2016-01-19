376819f04dc667bdb18b65740ff8dbfe21136803
Ben Van Houten <ben@benjamins-mbp.carolina.rr.com>
2016-01-19 17:58:40 -0500

    Initial Commit

---
+++ Flickr Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flickr Finder
+//
+//  Created by Benjamin Van Houten on 1/19/16.
+//  Copyright © 2016 Benjamin Van Houten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flickr FinderTests/Flickr_FinderTests.m
@@ -0,0 +1,39 @@
+//
+//  Flickr_FinderTests.m
+//  Flickr FinderTests
+//
+//  Created by Benjamin Van Houten on 1/19/16.
+//  Copyright © 2016 Benjamin Van Houten. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flickr_FinderTests : XCTestCase
+
+@end
+
+@implementation Flickr_FinderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
